5470fdd4f314751d7c7522f7835b7b0a1c49c335
Safwan Islam <saislam@davidson.edu>
2022-05-24 14:20:04 -0500

    changed prompt to confirms, simplified code

--- Develop/script.js
+++ Develop/script.js
@@ -10,58 +10,41 @@ function generatePassword() {
 
   var lengthInput = parseInt(prompt("Choose a length between 8 and 128"));
 
-  var lowercaseInput = prompt("Would you like to have lowercase letters"); 
-  lowercaseInput = lowercaseInput.toLowerCase()[0];
-
-  var numbersInput =  prompt("Would you like to have numbers");
-  numbersInput = numbersInput.toLowerCase()[0]; 
-
-  var specialInput = prompt("Would you like to have special characters")
-  specialInput = specialInput.toLowerCase()[0]; 
-
-  var uppercaseInput =  prompt("Would you like to have uppercase letters");
-  uppercaseInput = uppercaseInput.toLowerCase()[0];
-
-  password = ""
-  userChoice = ""
-
   if (lengthInput >= 8 && lengthInput <= 128) {
-    if (lowercaseInput == 'y'){
-      userChoice += lowercase; 
-      writePassword();
-    } else if (lowercaseInput == 'n'){
-      return;
-    } else {
-      alert("Please select a valid choice!");
-    }
-    if (numbersInput == 'y'){
-      userChoice += numbers; 
-      writePassword();
-    } else if (numbers == 'n'){
-      return;
-    } else {
-      alert("Please select a valid choice!");
-    }
-    if (specialInput == 'y'){
-      userChoice += special; 
-      writePassword();
-    } else if (specialInput == 'n'){
-      return;
-    } else {
-      alert("Please select a valid choice!");
-    }
-    if (uppercaseInput == 'y'){
-      userChoice += uppercase; 
-      writePassword();
-    } else if (uppercaseInput == 'n'){
-      return;
-    } else {
-      alert("Please select a valid choice!");
-    }
-  }
-  else {
+    console.log(lengthInput.length);
+  } else {
   alert("Not allowed, please select a password within the given length");
+    generatePassword();
   }
+
+  var lowercaseInput = confirm("Would you like to have lowercase letters"); 
+  var numbersInput =  confirm("Would you like to have numbers");
+  var specialInput = confirm("Would you like to have special characters")
+  var uppercaseInput = confirm("Would you like to have uppercase letters");
+  
+
+  password = ' ';
+  userChoice = ' ';
+
+
+
+  if(!lowercaseInput && !numbersInput && !specialInput && !uppercaseInput ){
+    alert("You must select one criteria")
+    writePassword();
+  }
+
+  if (lowercaseInput) {
+    userChoice += lowercase;
+  }    
+  if (numbersInput) {
+    userChoice += numbers;
+  }   
+  if (specialInput) {
+    userChoice += special;
+  }   
+  if (uppercaseInput) {
+    userChoice += uppercase;
+  }   
 
   for (var i=0; i<lengthInput; i++){
     var random = Math.floor(Math.random()*userChoice.length);
